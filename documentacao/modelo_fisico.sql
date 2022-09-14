cfbb08388cdb0accda68ea6c4112cfc673781560
Gian Luca Oliveira <gloliveira539@gmail.com>
2022-09-14 10:13:25 -0300

    Reescrevendo propriedades

--- documentacao/modelo_fisico.sql
+++ documentacao/modelo_fisico.sql
@@ -13,8 +13,8 @@ CREATE DATABASE auto_oficina
 
 CREATE TABLE public.Pessoa (
 	cpf varchar(11) NOT NULL,
-	first_name varchar(20) NOT NULL,
-	last_name varchar(60) NOT NULL,
+	nome varchar(20) NOT NULL,
+	sobrenome varchar(60) NOT NULL,
 	rg varchar(7) NOT NULL UNIQUE,
 	telefone varchar(11) NOT NULL,
 	CONSTRAINT Pessoa_pk PRIMARY KEY (cpf)
@@ -23,12 +23,12 @@ CREATE TABLE public.Pessoa (
 );
 
 
-CREATE TYPE categoria_cliente AS ENUM ('Esporadico', 'Especial', 'Mal_Pagador', 'Devedor');
+CREATE TYPE categoria_cliente AS ENUM ('esporadico', 'especial', 'mal_pagador', 'devedor');
 
 CREATE TABLE public.Cliente(
 	codigo serial NOT NULL,
 	cpf_pessoa varchar(11) NOT NULL UNIQUE,
-	tipo categoria_cliente NOT NULL DEFAULT 'Esporádico',
+	tipo categoria_cliente NOT NULL DEFAULT 'esporadico',
 	CONSTRAINT Cliente_pk PRIMARY KEY (codigo),
 	CONSTRAINT Cliente_fk0 FOREIGN KEY (cpf_pessoa) REFERENCES Pessoa(cpf)
 ) WITH (
@@ -60,19 +60,19 @@ CREATE TABLE public.Pessoa_Endereco (
 );
 
 
-CREATE TYPE setor_atuacao AS ENUM ('Administrativo', 'Operacional', 'Mecanico');
-CREATE TYPE tipo_conserto AS ENUM ('Geral','Oleo','Balanceamento','Embreagem','Freios','Radiador');
+CREATE TYPE setor_atuacao AS ENUM ('administrativo', 'operacional', 'mecanico');
+CREATE TYPE tipo_conserto AS ENUM ('geral','oleo','balanceamento','embreagem','freios','radiador');
 
 
 CREATE TABLE public.Funcionario (
-	id serial NOT NULL,
+	codigo serial NOT NULL,
 	cpf_pessoa varchar(11) NOT NULL UNIQUE,
 	url_foto TEXT NOT NULL UNIQUE,
 	data_admissao DATE NOT NULL,
 	salario FLOAT NOT NULL DEFAULT 1200,
 	setor setor_atuacao NOT NULL,
 	especialidade tipo_conserto,
-	CONSTRAINT Funcionario_pk PRIMARY KEY (id)
+	CONSTRAINT Funcionario_pk PRIMARY KEY (codigo)
 ) WITH (
   OIDS=FALSE
 );
@@ -125,13 +125,13 @@ CREATE TABLE public.Servico_Atividade (
 );
 
 
-CREATE TYPE status_geral AS ENUM ('Espera', 'Iniciado', 'Concluido');
+CREATE TYPE status_geral AS ENUM ('espera', 'iniciado', 'concluido');
 
 
 CREATE TABLE public.Ordem_Servico (
 	codigo serial NOT NULL,
 	placa_veiculo varchar(7) NOT NULL,
-	codigo_cliente varchar(11) NOT NULL,
+	codigo_cliente integer NOT NULL,
 	codigo_supervisor integer NOT NULL,
 	situacao status_geral NOT NULL DEFAULT 'concluido',
 	data_entrada DATE NOT NULL,
@@ -143,7 +143,7 @@ CREATE TABLE public.Ordem_Servico (
 );
 
 
-CREATE TYPE forma_pagamento AS ENUM ('Credito', 'Debito', 'Dinheiro', 'PIX');
+CREATE TYPE forma_pagamento AS ENUM ('credito', 'debito', 'dinheiro', 'pix');
 
 
 CREATE TABLE public.Pagamento (
@@ -165,7 +165,7 @@ CREATE TABLE public.Atividades_Realizadas (
 	codigo_ordem_servico integer NOT NULL,
 	codigo_servico integer NOT NULL,
 	codigo_atividade integer NOT NULL,
-	id_funcionario integer,
+	codigo_funcionario integer NOT NULL,
 	situacao status_geral NOT NULL DEFAULT 'concluido',
 	CONSTRAINT Atividades_Realizadas_pk PRIMARY KEY (codigo)
 ) WITH (
@@ -186,11 +186,11 @@ CREATE TABLE public.Unidade_Movel (
 
 CREATE TABLE public.Agendamento (
 	codigo serial NOT NULL,
-	cpf_cliente varchar(11) NOT NULL,
+	codigo_cliente integer NOT NULL,
 	placa_veiculo varchar(7) NOT NULL,
 	id_endereco integer NOT NULL,
 	data_hora TIMESTAMP NOT NULL,
-	situacao status_geral NOT NULL DEFAULT 'em espera',
+	situacao status_geral NOT NULL DEFAULT 'espera',
 	CONSTRAINT Agendamento_pk PRIMARY KEY (codigo),
 	CONSTRAINT Agendamento_fk3 FOREIGN KEY (placa_veiculo) REFERENCES Veiculo(placa)
 ) WITH (
@@ -198,7 +198,7 @@ CREATE TABLE public.Agendamento (
 );
 
 
-CREATE TYPE tipo_mensagem AS ENUM ('Iniciado','Rejeitado', 'Aceito', 'Finalizado', 'No_Local', 'Impossivel_Iniciar');
+CREATE TYPE tipo_mensagem AS ENUM ('iniciado','rejeitado', 'aceito', 'finalizado', 'impossivelIniciar');
 
 
 CREATE TABLE public.Mensagem_Terminal (
@@ -224,7 +224,7 @@ ALTER TABLE Pessoa_Veiculo ADD CONSTRAINT Pessoa_Veiculo_fk1 FOREIGN KEY (placa_
 
 ALTER TABLE Ordem_Servico ADD CONSTRAINT Ordem_Servico_fk0 FOREIGN KEY (placa_veiculo) REFERENCES Veiculo(placa);
 ALTER TABLE Ordem_Servico ADD CONSTRAINT Ordem_Servico_fk1 FOREIGN KEY (codigo_cliente) REFERENCES Cliente(codigo);
-ALTER TABLE Ordem_Servico ADD CONSTRAINT Ordem_Servico_fk2 FOREIGN KEY (id_supervisor) REFERENCES Funcionario(id);
+ALTER TABLE Ordem_Servico ADD CONSTRAINT Ordem_Servico_fk2 FOREIGN KEY (codigo_supervisor) REFERENCES Funcionario(codigo);
 
 ALTER TABLE Servico_Atividade ADD CONSTRAINT Servico_Atividade_fk0 FOREIGN KEY (codigo_servico) REFERENCES Servico(codigo);
 ALTER TABLE Servico_Atividade ADD CONSTRAINT Servico_Atividade_fk1 FOREIGN KEY (codigo_atividade) REFERENCES Atividade(codigo);
@@ -232,7 +232,7 @@ ALTER TABLE Servico_Atividade ADD CONSTRAINT Servico_Atividade_fk1 FOREIGN KEY (
 ALTER TABLE Atividades_Realizadas ADD CONSTRAINT Atividades_Realizadas_fk0 FOREIGN KEY (codigo_ordem_servico) REFERENCES Ordem_Servico(codigo);
 ALTER TABLE Atividades_Realizadas ADD CONSTRAINT Atividades_Realizadas_fk1 FOREIGN KEY (codigo_servico) REFERENCES Servico(codigo);
 ALTER TABLE Atividades_Realizadas ADD CONSTRAINT Atividades_Realizadas_fk2 FOREIGN KEY (codigo_atividade) REFERENCES Atividade(codigo);
-ALTER TABLE Atividades_Realizadas ADD CONSTRAINT Atividades_Realizadas_fk3 FOREIGN KEY (id_funcionario) REFERENCES Funcionario(id);
+ALTER TABLE Atividades_Realizadas ADD CONSTRAINT Atividades_Realizadas_fk3 FOREIGN KEY (codigo_funcionario) REFERENCES Funcionario(codigo);
 
 ALTER TABLE Agendamento ADD CONSTRAINT Agendamento_fk0 FOREIGN KEY (codigo_cliente) REFERENCES Cliente(codigo);
 ALTER TABLE Agendamento ADD CONSTRAINT Agendamento_fk1 FOREIGN KEY (placa_veiculo) REFERENCES Veiculo(placa);
